4ea1718792c770264afbd3d1806f2152b27dd3cc
alzh2572 <al.nk.zh@gmail.com>
2026-08-07 04:53:58 +0300

    Update greeting text in README.md and HomeScreen component to personalize the message.

--- app/index.tsx
+++ app/index.tsx
@@ -13,7 +13,7 @@ export default function HomeScreen() {
 
       <View style={styles.content}>
         <Text variant="headlineSmall" style={styles.title}>
-          Привет, React Native Paper!
+          Привет, Алексей!
         </Text>
         <Button mode="contained" onPress={() => setSnackbarVisible(true)}>
           Нажми меня
